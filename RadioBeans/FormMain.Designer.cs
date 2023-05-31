f2a726dfdd62b8dde246ea4ff0079653d892a6c6
RustyBeans <107262647+RustyBeans@users.noreply.github.com>
2023-05-30 19:34:17 -0700

    set up some test stuff like switching file type, opening the sound, and setting up the designer layout

--- RadioBeans/FormMain.Designer.cs
+++ RadioBeans/FormMain.Designer.cs
@@ -32,6 +32,9 @@
 			pictureBox1 = new PictureBox();
 			btnBitmapTest = new Button();
 			lblNowPlaying = new Label();
+			txtYoutubeURL = new TextBox();
+			lblTest = new Label();
+			cboFileTypeSelect = new ComboBox();
 			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
 			SuspendLayout();
 			// 
@@ -47,15 +50,15 @@
 			// 
 			// pictureBox1
 			// 
-			pictureBox1.Location = new Point(118, 32);
+			pictureBox1.Location = new Point(140, 12);
 			pictureBox1.Name = "pictureBox1";
-			pictureBox1.Size = new Size(553, 344);
+			pictureBox1.Size = new Size(925, 635);
 			pictureBox1.TabIndex = 1;
 			pictureBox1.TabStop = false;
 			// 
 			// btnBitmapTest
 			// 
-			btnBitmapTest.Location = new Point(701, 12);
+			btnBitmapTest.Location = new Point(12, 41);
 			btnBitmapTest.Name = "btnBitmapTest";
 			btnBitmapTest.Size = new Size(87, 23);
 			btnBitmapTest.TabIndex = 2;
@@ -65,17 +68,47 @@
 			// lblNowPlaying
 			// 
 			lblNowPlaying.AutoSize = true;
-			lblNowPlaying.Location = new Point(12, 426);
+			lblNowPlaying.Location = new Point(12, 69);
 			lblNowPlaying.Name = "lblNowPlaying";
 			lblNowPlaying.Size = new Size(122, 15);
 			lblNowPlaying.TabIndex = 3;
 			lblNowPlaying.Text = "Now Playing: fdffsdfd";
 			// 
+			// txtYoutubeURL
+			// 
+			txtYoutubeURL.Location = new Point(12, 87);
+			txtYoutubeURL.Name = "txtYoutubeURL";
+			txtYoutubeURL.Size = new Size(122, 23);
+			txtYoutubeURL.TabIndex = 4;
+			txtYoutubeURL.Text = "Enter URL...";
+			txtYoutubeURL.TextChanged += txtYoutubeURL_TextChanged;
+			// 
+			// lblTest
+			// 
+			lblTest.AutoSize = true;
+			lblTest.Location = new Point(12, 286);
+			lblTest.Name = "lblTest";
+			lblTest.Size = new Size(81, 15);
+			lblTest.TabIndex = 5;
+			lblTest.Text = "^TEST ZONE^";
+			// 
+			// cboFileTypeSelect
+			// 
+			cboFileTypeSelect.FormattingEnabled = true;
+			cboFileTypeSelect.Location = new Point(12, 116);
+			cboFileTypeSelect.Name = "cboFileTypeSelect";
+			cboFileTypeSelect.Size = new Size(121, 23);
+			cboFileTypeSelect.TabIndex = 6;
+			cboFileTypeSelect.SelectedIndexChanged += cboFileTypeSelect_SelectedIndexChanged;
+			// 
 			// FormMain
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(800, 450);
+			ClientSize = new Size(1077, 659);
+			Controls.Add(cboFileTypeSelect);
+			Controls.Add(lblTest);
+			Controls.Add(txtYoutubeURL);
 			Controls.Add(lblNowPlaying);
 			Controls.Add(btnBitmapTest);
 			Controls.Add(pictureBox1);
@@ -94,5 +127,8 @@
 		private PictureBox pictureBox1;
 		private Button btnBitmapTest;
 		private Label lblNowPlaying;
+		private TextBox txtYoutubeURL;
+		private Label lblTest;
+		private ComboBox cboFileTypeSelect;
 	}
 }
--- RadioBeans/FormMain.cs
+++ RadioBeans/FormMain.cs
@@ -1,42 +1,103 @@
+using LibVLCSharp.Shared;
+using System;
+using System.Linq;
 using System.Media;
+using System.Threading.Tasks;
+
 using System.Windows.Forms.Design;
 
 namespace RadioBeans
 {
+
 	public partial class FormMain : Form
 	{
-		String filePath = "";
+		string fileTypeSelected = string.Empty;
 		public FormMain()
 		{
 			InitializeComponent();
+
 		}
 
 		private void FormMain_Load(object sender, EventArgs e)
 		{
+			
+			String[] fileTypes =
+			{
+				"WAV files (*.wav)|*.wav",
+				"MP3 files (*.mp3)|*.mp3",
+				"OGG files (*.ogg)|*.ogg"
+			};
 
+			foreach (String type in fileTypes)
+			{
+				cboFileTypeSelect.Items.Add(type);
+			}
+			
+			/*cboFileTypeSelect.SelectedItem = 1;
+			cboFileTypeSelect.Items.Add("WAV files (*.wav)|*.wav");
+			cboFileTypeSelect.Items.Add("MP3 files (*.mp3)|*.mp3");
+			cboFileTypeSelect.Items.Add("OGG files (*.ogg)|*.ogg");
+*/
 		}
 
 		private void btnSoundTest_Click(object sender, EventArgs e)
 		{
-			OpenFileDialog ofd = new OpenFileDialog();
-			ofd.InitialDirectory = "D:\\Music";
-			ofd.Filter = "WAV files (*.WAV)|*.WAV|All files (*.*)|*.*";
-			ofd.FilterIndex = 1;
-			ofd.RestoreDirectory = true;
-
+			OpenFileDialog ofd = OpenFile();
 
 			if (ofd.ShowDialog() == DialogResult.OK)
 			{
-				
-				string fileName = ofd.SafeFileName;
-				filePath = ofd.FileName;
-				SoundPlayer soundPlayer = new SoundPlayer(filePath);
-				lblNowPlaying.Text = "Now Playing: " + fileName;
-				soundPlayer.Play();
+				string filePath = ofd.FileName;
+				SoundPlayer player = new SoundPlayer(filePath);
+				player.Play();
 			}
 
 		}
 
+		private void txtYoutubeURL_TextChanged(object sender, EventArgs e)
+		{
 
+		}
+
+		// GETTING MEDIA FILES
+		public OpenFileDialog OpenFile()
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.InitialDirectory = Environment.SpecialFolder.CommonMusic.ToString();
+			//ofd.InitialDirectory = "D:\\Design";
+			ofd.Filter = fileTypeSelected;
+			ofd.FilterIndex = 1;
+			ofd.RestoreDirectory = true;
+			return ofd;
+		}
+		public String GetFilePath(OpenFileDialog ofd)
+		// get the full file path of the desired file to be used by the media player internals
+		{
+			string filePath = ofd.FileName;
+			return filePath;
+		}
+		public String GetFileName(OpenFileDialog ofd)
+		// get the modified path to be used to display the title of the currently playing media
+		{
+			string fileName = ofd.SafeFileName;
+			return fileName;
+		}
+
+		// PLAYING SOUND
+		public SoundPlayer SelectSound(String filePath)
+		// pass in the file path of the desired audio and return the object with that information
+		{
+			SoundPlayer soundPlayer = new SoundPlayer(filePath);
+			return soundPlayer;
+		}
+		public void PlaySound(SoundPlayer soundPlayer)
+		// pass in the sound player and file path information and play that audio
+		{
+			soundPlayer.Play();
+		}
+
+		private void cboFileTypeSelect_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			fileTypeSelected = cboFileTypeSelect.SelectedItem.ToString();
+		}
 	}
 }
